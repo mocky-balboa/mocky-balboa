1789119b907b0632d3ae3e02d78593b79099edde
Lewis Nelson <lewis@lewnelson.com>
2025-09-02 11:39:35 +0100

    feat: added option to react-router cli to specify dist directory

--- packages/react-router/src/cli.ts
+++ packages/react-router/src/cli.ts
@@ -9,15 +9,28 @@ import {
 } from "@mocky-balboa/cli-utils";
 import { createRequestHandler } from "@react-router/express";
 
+interface CLIOptions {
+  distDir: string;
+}
+
+const DefaultDistDir = "build";
+
 const cli = createCommand(
   "mocky-balboa-react-router",
   "Starts a Node.js http server powered by Express for your React Router application as well as the necessary mocky-balboa servers",
 );
 
+cli.argument(
+  "[dist-dir]",
+  "Path to the directory where your React Router application is built",
+  DefaultDistDir,
+);
+
 const main = async () => {
-  const cliOptions = parseCLIOptions(cli);
+  const cliOptions = parseCLIOptions<CLIOptions>(cli);
+  const { distDir = DefaultDistDir } = cliOptions;
   const build = await getServerEntryHandler(
-    path.resolve(process.cwd(), "build"),
+    path.resolve(process.cwd(), distDir),
     ["server/index"],
   );
 
