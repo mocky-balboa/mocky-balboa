2c8a01d6c22d465f12e4261f144a8ffe42104a41
Lewis Nelson <lewis@lewnelson.com>
2025-09-02 15:20:41 +0100

    chore: better canary release pattern

---
+++ scripts/update-package-canary-versions.cjs
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = path.resolve(__dirname, "..");
+const packagesDir = path.join(rootDir, "packages");
+const items = fs.readdirSync(packagesDir);
+
+const commitHash = process.argv[2];
+
+if (typeof commitHash !== "string" || commitHash.length !== 7) {
+  console.error("Invalid commit hash");
+  process.exit(1);
+}
+
+for (const item of items) {
+  const packageDir = path.join(packagesDir, item);
+  const packageJsonPath = path.join(packageDir, "package.json");
+  if (!fs.existsSync(packageJsonPath)) continue;
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+  if (!packageJson.version.match(/-canary\.0$/)) {
+    continue;
+  }
+
+  const newVersion = packageJson.version.replace(
+    /-canary\.0$/,
+    `-canary.${commitHash}`,
+  );
+
+  console.log(`Updating ${packageJson.name} to ${newVersion}`);
+  packageJson.version = newVersion;
+  fs.writeFileSync(
+    packageJsonPath,
+    `${JSON.stringify(packageJson, null, 2)}\n`,
+  );
+}
