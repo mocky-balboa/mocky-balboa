959998aca9706e761d0cc8fe527aedeffad79acc
Lewis Nelson <lewis@lewnelson.com>
2025-09-07 21:44:09 +0100

    fix: use correct deployment url

--- scripts/comment-docs-preview-urls.cjs
+++ scripts/comment-docs-preview-urls.cjs
@@ -11,6 +11,18 @@ if (typeof commitHash !== "string" || commitHash.length !== 40) {
 
 const shortCommit = commitHash.substring(0, 7);
 
+const docsDeploymentUrl = process.env.DOCS_DEPLOYMENT_URL;
+const apiReferenceDocsDeploymentUrl = process.env.API_REFERENCE_DOCS_DEPLOYMENT_URL;
+if (!docsDeploymentUrl) {
+  console.error("Missing DOCS_DEPLOYMENT_URL env var");
+  process.exit(1);
+}
+
+if (!apiReferenceDocsDeploymentUrl) {
+  console.error("Missing API_REFERENCE_DOCS_DEPLOYMENT_URL env var");
+  process.exit(1);
+}
+
 const getCommentBody = () => {
   return `${docsCommentIdentifier}
 
@@ -19,8 +31,8 @@ const getCommentBody = () => {
 |- Name -|- Link -|
 |--------|--------|
 | Latest commit | [${shortCommit}](https://github.com/mocky-balboa/mocky-balboa/commit/${commitHash}) |
-| Docs preview | [https://${shortCommit}.mocky-balboa.pages.dev](https://${shortCommit}.mocky-balboa.pages.dev) |
-| API reference docs preview | [https://${shortCommit}.mocky-balboa-api-reference.pages.dev](https://${shortCommit}.mocky-balboa-api-reference.pages.dev) |
+| Docs preview | [${docsDeploymentUrl}](${docsDeploymentUrl}) |
+| API reference docs preview | [${apiReferenceDocsDeploymentUrl}](${apiReferenceDocsDeploymentUrl}) |
   `
 };
 
