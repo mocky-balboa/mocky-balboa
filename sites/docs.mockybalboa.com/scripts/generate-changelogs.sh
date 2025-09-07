7a91c4690584607632286482cc944a7bc4d50a98
Lewis Nelson <lewis@lewnelson.com>
2025-09-07 16:16:59 +0100

    feat: migrating docs to cloudflare

--- sites/docs.mockybalboa.com/scripts/generate-changelogs.sh
+++ sites/docs.mockybalboa.com/scripts/generate-changelogs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ -z "$CONTEXT" ]; then
-  echo "Expected Netlify build env CONTEXT. Exiting."
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ -z "$BRANCH" ]; then
+  echo "Unable to determine current git branch. Exiting."
   exit 1
 fi
 
-if [ "$CONTEXT" == "production" ]; then
+if [ "$BRANCH" == "main" ]; then
   echo "Generating changelogs for main branch"
   pnpm -w exec changeset version
 else
-  echo "Generating changelogs for branch $CONTEXT"
+  echo "Generating changelogs for branch $BRANCH"
   pnpm -w exec changeset version --snapshot canary
 fi
