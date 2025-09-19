aaac5f356760f64955fce6cb31206e60a45a69a1
Lewis Nelson <lewis@lewnelson.com>
2025-09-19 22:15:38 +0100

    feat(shared-config): added new config related to proxy server and sse

--- packages/shared-config/src/shared-config.ts
+++ packages/shared-config/src/shared-config.ts
@@ -1,3 +1,9 @@
 export const DefaultWebSocketServerPort = 58152;
+export const DefaultProxyServerPort = 58153;
 export const UnsetClientIdentity = "not-set";
 export const ClientIdentityStorageHeader = "x-mocky-balboa-client-id";
+export const SSEProxyRequestIdParam = "mocky-balboa-sse-proxy-request-id";
+export const SSEProxyOriginalUrlParam = "mocky-balboa-sse-proxy-original-url";
+export const SSEProxyEndpoint = "/__mocky_balboa_internal_sse_proxy_endpoint__"
+export const BrowserGetSSEProxyParamsFunctionName = "__mocky_balboa_client_sse";
+
